2d2b055b5ff7fef6d05a158c9adf04b604fee459
SungCoders <levansung28@gmail.com>
2022-05-26 16:14:37 +0700

    this is file c

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int numa;
+    printf("nhap so nguyen: ");
+    scanf("%d", &numa);
+    printf("\nso nguyen ban vua nhap la: %d", numa);
+    return 0;
+}
